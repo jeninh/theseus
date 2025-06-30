a04605d666ca2eccad9e59b9a748269b6129cf0b
24c02 <163450896+24c02@users.noreply.github.com>
2025-06-30 15:07:33 -0400

    fix half_letter template

--- app/lib/snail_mail/components/half_letter_component.rb
+++ app/lib/snail_mail/components/half_letter_component.rb
@@ -11,11 +11,27 @@ module SnailMail
 
       def view_template
         render_front
+        start_new_page
+        render_back
       end
 
       # Override in subclasses to define the front content
       def render_front
         raise NotImplementedError, "Subclasses must implement render_front"
+      end
+
+      def render_back
+        render_postage
+        render_imb(bounds.right - 217, 25, 207)
+        render_return_address(5, bounds.top - 5, 190, 90, size: 8, font: "f25")
+        render_destination_address(
+          (bounds.right/2)-256/2,
+          (bounds.top/2)+107/2,
+          256,
+          107,
+          size: 18, valign: :center, align: :left
+        )
+        render_qr_code(2, 52, 50)
       end
     end
   end
