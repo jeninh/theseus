eac12782ab72017ec3e7e014c2ffb73e25c31ac9
24c02 <163450896+24c02@users.noreply.github.com>
2025-09-23 16:14:18 -0400

    formerly chuck's

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,22 @@ SourceTag.find_or_create_by!(
   owner: "Nora",
 )
 
+ReturnAddress.find_or_create_by!(id: 1) do |address|
+  address.name = "Hack Club"
+  address.line_1 = "15 Falls Rd."
+  address.city = "Shelburne"
+  address.state = "VT"
+  address.postal_code = "05482"
+  address.country = "US"
+  address.shared = true
+end
+
+USPS::MailerId.find_or_create_by(id: 1) do |mid|
+  mid.name = "Hack Club (Shelburne)"
+  mid.crid = "40039652"
+  mid.mid = "903258919"
+end
+
 # Warehouse::PurposeCode.find_or_create_by!(
 #   code: Rails.env.production? ? 'HQ' : 'HQ-dev',
 #   description: 'general HQ mailing'
