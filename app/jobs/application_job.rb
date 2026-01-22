d52e934c2bce871c9dba85fc0765c1ef3d66a6af
24c02 <163450896+24c02@users.noreply.github.com>
2026-01-22 16:03:57 -0500

    concursedency

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,4 +1,6 @@
 class ApplicationJob < ActiveJob::Base
+  include GoodJob::ActiveJobExtensions::Concurrency
+
   # Automatically retry jobs that encountered a deadlock
   # retry_on ActiveRecord::Deadlocked
 
